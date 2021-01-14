a73b52b7500527d4957b68aa4fa7032eb4da2300
DorfDork <nickfgh@gmail.com>
2021-01-14 14:51:15 -0600

    Main + HUD Font Tables Cleanup from gammatendonnine.

--- tools/text2ascii.py
+++ tools/text2ascii.py
@@ -7,7 +7,7 @@ import glob
 import re
 from pathlib import Path
 
-path = "/home/alex/Documents/Projects/Render96/Render96ex - FastBuild/charmap.txt"
+path = "charmap.txt"
 lines = open(os.path.abspath(path), 'r').readlines()
 
 lineID = {}
